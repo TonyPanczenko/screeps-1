8f79ab3a38dd12168b7360797077fd27ee0ccf9d
Tony Panczenko <tony.panczenko@gmail.com>
2023-02-05 12:45:17 +0000

    implement deployment via api

--- bin/deploy.js
+++ bin/deploy.js
@@ -2,30 +2,33 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import fse from 'fs-extra';
 
-dotenv.config({ path: '../.env.local' });
+dotenv.config({ path: '.env.local' });
 
-const email = process.env.EMAIL;
-const password = process.env.PASSWORD;
-
-try {
-  const code = fse.readFileSync('../dist/app.js', { encoding: 'utf-8' });
-  const data = {
-    branch: 'default',
-    modules: {
-      'main.js': code,
-    },
-  };
-
-  axios.post(
-    process.env.API_URL,
-    data,
-    {
-      auth: `${email}:${password}`,
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
+async function deploy() {
+  try {
+    const code = fse.readFileSync('dist/app.js', { encoding: 'utf-8' });
+    const data = {
+      branch: 'develop',
+      modules: {
+        main: code,
       },
-    },
-  );
-} catch (e) {
-  console.log(`# Failed to deploy bundle to Screeps Server: ${e.message}`);
+    };
+
+    await axios.post(
+      process.env.API_URL,
+      data,
+      {
+        headers: {
+          'X-Token': process.env.TOKEN,
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+      },
+    );
+
+    console.log('# Successfully deployed to Screeps Server');
+  } catch (e) {
+    console.log(`# Failed to deploy bundle to Screeps Server: ${e.message}`);
+  }
 }
+
+deploy();
